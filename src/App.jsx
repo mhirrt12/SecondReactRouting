22c06fad9680d90f90d7edfabf2d8bae22aa8061
mhirrt12 <kaletamene90@gmail.com>
2026-03-19 02:03:35 -0700

    Updated App.jsx and added NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import Notfound from "./pages/Notfound";
 function App() {
   return (
     <Routes>
@@ -13,6 +13,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
       </Route>
     </Routes>
   );
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Notfound() {
+  return (
+  <h1>Sorry😒, The Page Is Found</h1>
+  )
+}
